def9e465b92294595d09d7ba54df219f2b9a26cb
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2020-10-15 19:30:43 +0600

    initial commit

---
+++ src/test/java/io/touhidjisan/projectmtool/ProjectmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.touhidjisan.projectmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
