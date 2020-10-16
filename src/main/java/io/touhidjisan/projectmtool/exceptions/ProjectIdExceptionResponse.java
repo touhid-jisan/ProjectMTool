8c5587f770d30265ebe0b8896ea5ef32c1246246
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2020-10-16 16:26:36 +0600

    custom exceptions for unique project identifier

---
+++ src/main/java/io/touhidjisan/projectmtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,18 @@
+package io.touhidjisan.projectmtool.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
--- src/main/java/io/touhidjisan/projectmtool/services/ProjectService.java
+++ src/main/java/io/touhidjisan/projectmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.touhidjisan.projectmtool.services;
 
 import io.touhidjisan.projectmtool.domain.Project;
+import io.touhidjisan.projectmtool.exceptions.ProjectIdException;
 import io.touhidjisan.projectmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,14 @@ public class ProjectService {
     }
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
     }
 
 }
