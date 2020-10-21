020975a994717de1118e4d68f25d5d485fe54e59
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2020-10-21 21:33:22 +0600

    get projects (redux only)

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { createProject } from "../../actions/projectaAction";
+import { createProject } from "../../actions/projectAction";
 import classnames from 'classnames'
 
 class AddProject extends Component {
