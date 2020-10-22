5c7a150caf13bb69b2b2b304948af72f8095945d
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2020-10-22 20:11:16 +0600

    update project connection to db

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,6 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { getProject, createProject } from "../../actions/projectAction";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
-export default class UpdateProject extends Component {
+class UpdateProject extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: "",
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      start_date: "",
+      end_date: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      start_date,
+      end_date
+    } = nextProps.project;
+
+    this.setState({
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      start_date,
+      end_date
+    });
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+      e.preventDefault();
+      const updateProject = {
+        id: this.state.id,
+        projectName:  this.state.projectName,
+        projectIdentifier: this.state.projectIdentifier,
+        description: this.state.description,
+        start_date: this.state.start_date,
+        end_date: this.state.end_date
+      };
+
+      this.props.createProject(updateProject, this.props.history)
+  }
+
     render() {
         return (
         <div>
@@ -8,7 +71,7 @@ export default class UpdateProject extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
-                            <h5 className="display-4 text-center">Create Edit Project form</h5>
+                            <h5 className="display-4 text-center">Update Form</h5>
                             <hr />
                             <form onSubmit={this.onSubmit}> 
                                 <div className="form-group">
@@ -16,23 +79,27 @@ export default class UpdateProject extends Component {
                                     type="text" 
                                     className="form-control form-control-lg" 
                                     placeholder="Project Name" 
-                                    name="projectName" />
-                                </div>
-                               
+                                    name="projectName"
+                                    value={this.state.projectName}
+                                    onChange ={this.onChange}/>
+                                </div>                              
                                 
                                 <div className="form-group">
                                     <input 
                                     type="text" 
                                     className="form-control form-control-lg" 
                                     placeholder="Unique Project ID" disabled
-                                    name="projectIdentifier" />
+                                    name="projectIdentifier" 
+                                    value={this.state.projectIdentifier}/>
                                 </div>
                                 
                                 <div className="form-group">
                                     <textarea 
                                     className="form-control form-control-lg"
                                     placeholder="Project Description"
-                                    name="description" >
+                                    name="description" 
+                                    value={this.state.description}
+                                    onChange ={this.onChange}>
                                     </textarea>
                                 </div>
                                
@@ -41,14 +108,18 @@ export default class UpdateProject extends Component {
                                     <input 
                                     type="date" 
                                     className="form-control form-control-lg" 
-                                    name="start_date"/>
+                                    name="start_date"
+                                    value={this.state.start_date}
+                                    onChange ={this.onChange}/>
                                 </div>
                                 <h6>Estimated End Date</h6>
                                 <div className="form-group">
                                     <input 
                                     type="date" 
                                     className="form-control form-control-lg" 
-                                    name="end_date" />
+                                    name="end_date"
+                                    value={this.state.end_date} 
+                                    onChange ={this.onChange}/>
                                 </div>
 
                                 <input 
@@ -63,3 +134,17 @@ export default class UpdateProject extends Component {
         )
     }
 }
+UpdateProject.propTypes = {
+    getProject: PropTypes.func.isRequired,
+    project: PropTypes.object.isRequired,
+    createProject: PropTypes.func.isRequired
+  };
+  
+  const mapStateToProps = state => ({
+    project: state.project.project
+  });
+  
+  export default connect(
+    mapStateToProps,
+    { getProject , createProject}
+  )(UpdateProject);
